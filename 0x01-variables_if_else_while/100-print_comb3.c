0e4e69527dab41950bd95a21e4c5d1d374be2ed1
Clarence Nwokocha <clarevission@gmail.com>
2022-12-12 15:23:27 -0800

    Print combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c = 0;
+
+	while (c < 10)
+	{
+		putchar(48 + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
