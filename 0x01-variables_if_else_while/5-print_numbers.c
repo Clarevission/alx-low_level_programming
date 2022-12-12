967837518857bb71814458c5d538085d9a7542d3
Clarence Nwokocha <clarevission@gmail.com>
2022-12-12 05:16:11 -0800

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,6 +4,7 @@
 /**
  * main - main block
  * Description: print all single digit numbers fo base 10
+ * starting from 0, followed by a new line.
  * Return: 0
  */
 int main(void)
@@ -15,7 +16,6 @@ int main(void)
 		putchar(c);
 		c++;
 	}
-
 	putchar('\n');
 	return (0);
 }
