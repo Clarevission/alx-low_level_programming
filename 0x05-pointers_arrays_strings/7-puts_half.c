e260e462ea5d625481be16aabf28b17e31107ee0
Clarence Nwokocha <clarevission@gmail.com>
2022-12-19 19:19:02 -0800

    Puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,11 +8,18 @@
 
 void puts_half(char *str)
 {
-	int i;
+	int i = 0;
+	int n = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
+		i += 1;
+	n = i / 2;
+	if (i % 2 == 1)
+		n += 1;
+	while (str[n] != '\0')
 	{
-		_putchar(str[i]);
+		_putchar(*(str + n));
+		n++;
 	}
 	_putchar('\n');
 }
