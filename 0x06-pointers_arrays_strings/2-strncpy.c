0fc94bcab493a03e29d74768cd968556aaa45be5
Clarence Nwokocha <clarevission@gmail.com>
2022-12-21 18:19:28 -0800

    Copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Retrun: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	_strncpy(dest, src, n);
+	return (dest);
+}
