9b204f96b411cc1195162de242bb70d0447c704f
Clarence Nwokocha <clarevission@gmail.com>
2023-01-13 15:37:38 -0800

    Multiplies two positive numbers

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,11 +5,13 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 void simple_print_buffer(char *buffer, unsigned int size);
-int main(void);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 void errors(void);
 int is_digit(char *s);
+void _is_zero(char *argv[]);
+char *_initialize_array(char *ar, int lar);
+int _checknum(char *argv[], int n);
 
 #endif
