8112ac19931a74aa4ac6fa2848fbe89da8bc2876
Clarence Nwokocha <clarevission@gmail.com>
2023-01-31 17:16:51 -0800

    Print safe version

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -66,6 +66,7 @@ size_t print_listint_safe(const listint_t *head)
 		for (; head != NULL; nodes++)
 		{
 			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
 
@@ -76,6 +77,7 @@ size_t print_listint_safe(const listint_t *head)
 			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
 		}
+
 		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
